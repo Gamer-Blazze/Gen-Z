354c27299d6522fb0303bde7b58b243c81b0a90d
vly.ai <support@vly.ai>
2025-09-11 08:43:21 +0000

    Revert: Remove TopNav component from Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -147,8 +147,6 @@ export default function Dashboard() {
     >
       {/* Global mobile nav bar removed on Dashboard */}
       {/* Add top navigation bar */}
-      <TopNav />
-
       {/* Background notifications watcher */}
       <NotificationWatcher />
 
